6faa9149bfabcb74e50f634ac518910c2251f92b
Jonathan Doherty <dohjon@users.noreply.github.com>
2024-07-14 22:31:51 +0200

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... } @ inputs: let
+    mkNixos = host: system:
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {inherit (self) inputs outputs;};
+        modules = [
+          ./hosts/${host}
+        ];
+      };
+  in {
+    nixosConfigurations = {
+      nixos = mkNixos "nixos" "x86_64-linux";
+    };
+  };
+}
